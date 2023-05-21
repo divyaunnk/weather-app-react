d596ffb13d8a62c36a7f85927b34ca087ef07e94
Divya Unnikrishnan <unnikrishnan.d@northeastern.edu>
2023-05-20 21:53:12 -0400

    Initial scaffolding

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TopButton from './components/TopButton';
+import Inputs from './components/Inputs';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+
+function App() {
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-600">
+      <TopButton />
+      <Inputs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Inputs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { UilAirplay } from '@iconscout/react-unicons';
+
+
+function Inputs() {
+  return (
+    <div>Inputs</div>
+  )
+}
+
+export default Inputs
